7d389391e11025cc59dda60007629112f92abe9f
pshenopro <n.psheno@gmail.com>
2024-03-24 19:34:49 +0300

    dev

--- components/StartContainer.vue
+++ components/StartContainer.vue
@@ -1,11 +1,11 @@
 <script setup>
   import { ref } from "@vue/reactivity"
 
-    import Arrow from '../assets/icons/arrow.vue'
-    import Vk from '../assets/icons/Vk.vue'
-    import Dzen from '../assets/icons/Dzen.vue'
-    import YouTube from '../assets/icons/YouTube.vue'
-    import Telgr from '../assets/icons/Telgr.vue'
+    import Arrow from './assets/icons/arrow.vue'
+    import Vk from './assets/icons/Vk.vue'
+    import Dzen from './assets/icons/Dzen.vue'
+    import YouTube from './assets/icons/YouTube.vue'
+    import Telgr from './assets/icons/Telgr.vue'
 
     const slides = [
         {
