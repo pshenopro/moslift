2d1c2eeb45e5808fff1be2c6b887191d664396e4
pshenichny <pshenichny@upt24.ru>
2024-06-04 17:58:54 +0300

    dev

--- server/index.js
+++ server/index.js
@@ -10,31 +10,31 @@ const bot = new Telegraf('7013399013:AAEufPc3IsYlRtB6sCEz06YOUJZAUS6TZF4', {
   handlerTimeout: Infinity,
 })
 
-const USER = {
-  chatId: '',
-  name: ''
-}
+const USERS = []
 
 bot.command('start', ctx => {
   // ctx.chat.id
-  ctx.reply(`Здарова: ${ctx.from.first_name || ctx.from.username} ❤`);
+  if ('blya_zaebal1' === ctx.from.username) {
+    ctx.reply(`НУ Здарова гандосик ${ctx.from.first_name || ctx.from.username} ❤, не думал такого педика тут встретить!`);
+  } else if ('cult_tape' === ctx.from.username) {
+    ctx.reply(`Ну привет Даха какаха))) ❤`);
+  } else {
+    ctx.reply(`Здарова: ${ctx.from.first_name || ctx.from.username} ❤`);
+  }
+
+  ctx.reply('Если хочешь узнать что я умею делать то введи /bot')
   const userId = ctx.from.id;
   const username = ctx.from.username || 'No username';
   const firstName = ctx.from.first_name || 'No first name';
   const lastName = ctx.from.last_name || 'No last name';
 
-  USER.chatId = ctx.chat.id
-  USER.name = ctx.from.first_name || ctx.from.username
-
-  // const message = `
-  //   Here is your account information:
-  //   - ID: ${userId}
-  //   - Username: ${username}
-  //   - First Name: ${firstName}
-  //   - Last Name: ${lastName}
-  //   `;
-  //
-  // ctx.reply(message);
+  if (!ctx.from.is_bot) {
+    USERS.push({
+      chatId: ctx.chat.id,
+      name: ctx.from.first_name || ctx.from.username,
+      user: ctx.from.username
+    })
+  }
 })
 
 bot.on(message('text'), ctx => {
@@ -60,14 +60,27 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }))
 
 server.post('/email', (req, res) => {
   console.log(req.body);
-  const messageText = `Hello! ${ USER.name}`
-  bot.telegram.sendMessage(USER.chatId, messageText)
-    .then(() => {
-      console.log(`Message sent to chat ID: ${ USER.chatId}`);
+  if (!USERS.length) {
+    res.status(200).send('DONE')
+  }
+
+  const posts = new Promise((res, rej) => {
+    USERS.forEach((user, i) => {
+      const messageText = `
+    Hello! ${ user.name};
+    - ${req.body.subject}
+    `
+      bot.telegram.sendMessage(user.chatId, messageText)
+
+      console.log(USERS.length === ++i)
+
+      if (USERS.length === ++i) {
+        res()
+      }
     })
-    .catch((err) => {
-      console.error(`Failed to send message to chat ID: ${ USER.chatId}. Error: ${err}`);
-    }).finally(() => {
+  })
+
+  posts.then(() => {
     res.status(200).send('DONE')
   })
 })
